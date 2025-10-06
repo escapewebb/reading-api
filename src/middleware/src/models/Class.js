7f7c892d480addfdc9387168d48f47fb99068b37
escapewebb <escapewebb1@gmail.com>
2025-10-05 22:48:30 -0500

    Create Class.js

---
+++ src/middleware/src/models/Class.js
@@ -0,0 +1,8 @@
+import mongoose from 'mongoose';
+const ClassSchema = new mongoose.Schema({
+  classCode: { type: String, unique: true, index: true, required: true },
+  name: { type: String, required: true },
+  grade: { type: String },
+  createdAt: { type: Date, default: Date.now }
+});
+export default mongoose.model('Class', ClassSchema);
