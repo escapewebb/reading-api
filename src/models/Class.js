2a9c1146d0066086be9f5d667b71c0e4af15bae9
escapewebb <escapewebb1@gmail.com>
2025-10-06 23:33:21 -0500

    Update Class.js

--- src/models/Class.js
+++ src/models/Class.js
@@ -1,8 +1,10 @@
 import mongoose from 'mongoose';
+
 const ClassSchema = new mongoose.Schema({
   classCode: { type: String, unique: true, index: true, required: true },
   name: { type: String, required: true },
   grade: { type: String },
   createdAt: { type: Date, default: Date.now }
 });
+
 export default mongoose.model('Class', ClassSchema);
